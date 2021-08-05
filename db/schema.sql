c1cfeff90715585ed97d45fedf3b31f67b3fd6c4
Kinley Davis <d8tascientist@gmail.com>
2021-08-05 10:40:44 -0400

    first steps of schema code added

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+-- Drop ccbbra_db if one already exists --
+DROP DATABASE IF EXISTS ccbbra_db;
+
+-- Create ccbbra_db database --
+CREATE DATABASE ccbbra_db;
+
+-- Set the following code to use the ccbbra_db database --
+USE ccbbra_db;
+
