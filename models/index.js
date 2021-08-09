1603b5f5c92e1197371546eb09efc2a0aaf098f0
Kinley Davis <d8tascientist@gmail.com>
2021-08-09 19:49:06 -0400

    added table relationships

--- models/index.js
+++ models/index.js
@@ -1,4 +1,3 @@
-
 const agency = require('./agency');
 const employee = require('./employee');
 const customer = require('./customer');
@@ -6,6 +5,55 @@ const accounting = require('./accounting');
 const vehicle = require('./vehicle');
 const reservation = require('./reservation');
 
+// AGENCY (1) : EMPLOYEE (*)
+agency.hasMany(employee, {
+    foreignKey: 'agency_id',
+    onDelete: 'CASCADE'
+});
+
+employee.belongsTo(agency, {
+    foreignKey: 'agency_id'
+});
+
+// AGENCY (1): VEHICLE (*)
+agency.hasMany(vehicle, {
+    foreignKey: 'agency_id',
+    onDelete: 'CASCADE'
+});
+
+vehicle.belongsTo(agency, {
+    foreignKey: 'agency_id'
+});
+
+// VEHICLE (1) : RESERVATION (*)
+vehicle.hasMany(reservation, {
+    foreignKey: 'vehicle_id',
+    onDelete: 'CASCADE'
+});
+
+reservation.belongsTo(vehicle, {
+    foreignKey: 'vehicle_id'
+});
+
+// CUSTOMER (1) : RESERVATION (*)
+customer.hasMany(reservation, {
+    foreignKey: 'customer_id'
+    onDelete: 'CASCADE'
+});
+
+reservation.belongsTo(customer, {
+    foreignKey: 'customer_id'
+});
+
+// CUSTOMER (1) : ACCOUNTING (*)
+customer.hasMany(accounting, {
+    foreignKey: 'customer_id',
+    onDelete: 'CASCADE'
+});
+
+accounting.belongsTo(customer, {
+    foreignKey: 'customer_id'
+});
 
 module.exports = { agency, employee, customer, accounting, vehicle, reservation };
 
