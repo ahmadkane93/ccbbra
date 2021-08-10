1978789b81c078de4e073f066307ddbab384967d
Nicholas Herold <nherold86@gmail.com>
2021-08-09 20:00:21 -0400

    create user and get user controler

--- controllers/api/customerRoutes.js
+++ controllers/api/customerRoutes.js
@@ -1,21 +1,36 @@
 const router = require ('express').Router();
-const { customer } = require('../../models')
+
+const { customer, reservation, } = require('../../models')
 
 router.post('/', async (req, res) => {
     try {
       const customerData = await customer.create(req.body);
   
-    //   req.session.save(() => {
-    //     req.session.user_id = userData.id;
-    //     req.session.logged_in = true;
-  
-    //     res.status(200).json(userData);
-    //   });
     res.status(200).json(customerData);
     } catch (err) {
       res.status(400).json(err);
     }
   });
+
+  router.get('/myres/:id', async(req,res) => {
+    try {
+        const customerData = await customer.findByPk(req.params.id
+        // ,{
+        //   include: [{model: reservation}]
+        // }
+        );
+        
+        return res.json(customerData);
+    }
+    catch (error) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
+
+  
 
 
   module.exports = router;
