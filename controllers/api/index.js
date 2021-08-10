0f2e89bf6d82d03602e1e1a5940ec46462598405
Kinley Davis <d8tascientist@gmail.com>
2021-08-09 20:42:32 -0400

    added reservation route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const customerRoutes = require('./customerRoutes');
-
-
+const reservationRoutes = require('./reservationRoute');
 
 router.use('/customer',customerRoutes);
-
+router.use('/reservation', reservationRoutes);
 
 module.exports = router;
