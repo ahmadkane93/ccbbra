9cdf979d1992e680c663879a566cdcf9ed6a3d52
Nicholas Herold <nherold86@gmail.com>
2021-08-10 21:42:27 -0400

    started installing and configuring passport for authentication

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,5 +5,11 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/profile', (req, res) => {
+    res.render('profile');
+});
+
+
+
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,22 @@
 const signupFormHandler = async (event) => {
     event.preventDefault();
   
-    const firstname = document.querySelector('#firstname-signup').value.trim();
+    const first_name = document.querySelector('#firstname-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    const lastname = document.querySelector('#lastname-signup').value.trim();
-    console.log(firstname)
-    if (firstname && email && password) {
+    const last_name = document.querySelector('#lastname-signup').value.trim();
+    const address = document.querySelector('#address-signup').value.trim();
+    const state = document.querySelector('#state-signup').value.trim();
+    const zip = document.querySelector('#zip-signup').value.trim();
+    const phone = document.querySelector('#phone-signup').value.trim();
+    const city = document.querySelector('#city-signup').value.trim();
+    
+  
+    
         
       const response = await fetch('/api/customer', {
         method: 'POST',
-        body: JSON.stringify({ firstname, lastname, email, password }),
+        body: JSON.stringify({ first_name, last_name, email, password,address,state,zip,phone,city}),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -19,7 +25,7 @@ const signupFormHandler = async (event) => {
       } else {
         alert(response.statusText);
       }
-    }
+    
   };
 
 
