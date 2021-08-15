fdeec3abeb311a7bc3ef4e92a06b241dce89ba71
Nicholas Herold <nherold86@gmail.com>
2021-08-14 23:45:13 -0400

    Modified Carlist page to recieve user data and a datepicker

--- config/auth.js
+++ config/auth.js
@@ -2,13 +2,11 @@
 module.exports={
   
    ensureAuthenticated: function(req, res, next) {
-     console.log('onemore')
       if (req.isAuthenticated()) {
         return next();
         
       }
       req.flash('error_msg', 'Please log in to view that resource');
-      console.log("password")
       res.redirect('/');
     },
     forwardAuthenticated: function(req, res, next) {
